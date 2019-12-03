e71854ba61256e20750b3b273d4b8aab99620eea
Jonathan Giler <jonathangiler@hotmail.comgit config --global user.name jonathan>
2019-12-03 14:29:05 -0500

    added build for https

---
+++ client/build/precache-manifest.6b75a7d79845017adbb6e0af6ddc7205.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b0b53c53bd46a3968964484d35ec27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "960192c814aca584f07a",
+    "url": "/static/css/2.570accbe.chunk.css"
+  },
+  {
+    "revision": "12bf5f6b7ab79eda532e",
+    "url": "/static/css/main.2ed7f05e.chunk.css"
+  },
+  {
+    "revision": "960192c814aca584f07a",
+    "url": "/static/js/2.0e1a881f.chunk.js"
+  },
+  {
+    "revision": "12bf5f6b7ab79eda532e",
+    "url": "/static/js/main.4b6bab33.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  },
+  {
+    "revision": "87d0efd3bf297dd2b568cc8bb9cb722e",
+    "url": "/static/media/JonathanGiler.87d0efd3.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
